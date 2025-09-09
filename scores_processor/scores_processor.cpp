465d66e99f9155bae27097458386653d40572c25
Benjamin <153864244+pybenny@users.noreply.github.com>
2025-09-09 00:39:38 -0500

    Full Pseudocode added
    Almost the rest of the program is outlined with pseudocode. Comments explaining tomorrows implementation.

--- scores_processor/scores_processor.cpp
+++ scores_processor/scores_processor.cpp
@@ -65,34 +65,39 @@ int main(int argc, char* argv[]) // argument count (argc), argument vector (argv
     // checking for fail to open
     if (!in_file) {
         cout << "ERROR: Input file has failed to open\n";
-        delete[] sArray;
+        delete[] sArray; // exit case | delete array preventing memory leaks
         return 1;
     }
     else {
         // after loading scores, the array and it's elements will be filled and ready for multi function use
         load_scores(in_file, sArray, size, capacity); // call on load_scores(); first
-        // call all other functions, total, min, max, by using the values stored in the array
+        // call all other statistical functions: total, min, max, by using the values stored in the array obtained by load_scores
         // call on compute average
         // call on count_above and count_below USING compute_average function within both of those two functions
-        // write all of the lines to out_file
+        // write all of the lines to out_file after calling on functions above
     }
 
-    in_file.close();
-    out_file.close();
-    delete [] sArray;
+    in_file.close(); // close input file
+    out_file.close(); // close output file
+    delete [] sArray; // delete array
     return 0;
 }
 
+// No printing. No stats. No opening/closing files. Returns when EOF reached.
+// only function that reads the file input.txt
 // only function that will resize, using resizable_mechanism
 // Loads all scores, if the size of the array is the same as capacity it will execute the resizable_mechanism function
 void load_scores(ifstream& in_file, int*& sArray, int& size, int& capacity) {
-    // might need the file input ifstream code somewhere here?
-    // read numbers(tokens) not text lines
+    // TEMPORARY: might need the file input ifstream code somewhere here? maybe not
+    // read the scores as numbers(tokens), not text lines.
 
+    // TEMP: NEED to implement appending here...
     if (size == capacity) {
         resizable_mechanism(sArray, size, capacity);
+        // double and pointer copy here
+        // append: write at logical end, address arr + size | pointer arithmetic
     }
-    ++size;
+    ++size; // ++size;
 }
 // currently only grows, needs to append a value, this function scales the array making room for all scores inside input.exe
 void resizable_mechanism(int*& sArray, int& size, int& capacity) {
@@ -115,8 +120,8 @@ void resizable_mechanism(int*& sArray, int& size, int& capacity) {
     //*(arrSize + );
 }
 
-//Count the total number of scores in input.txt
-/*void total_scores() {
+// iterates through the array that was filled by load_scores() func, and gives the total int size; amount of values in that array
+/*void total_scores() { // never resize, never read the file | only read the array | does not call load_scores, only the array that's filled
     int size = size;
 
     for (int i = 0; i < size; ++i) {
@@ -125,17 +130,14 @@ void resizable_mechanism(int*& sArray, int& size, int& capacity) {
 } */
 
 //Find the highest and lowest scores in input.txt || maybe need 2 seperate functions for this
-//void highest_score(){}
+//void highest_score(){} // never resize, never read the file | only read the array | does not call load_scores, only the array that's filled
 
-//void lowest_score(){}
+//void lowest_score(){} // never resize, never read the file | only read the array | does not call load_scores, only the array that's filled
 
 //Compute the average score in input.txt
-//void compute_average(){}
+//void compute_average(){} // never resize, never read the file | only read the array | does not call load_scores, only the array that's filled
 
 //Count how many scores are above average and how many are below average || maybe need 2 seperate functions for this
-//void above_average(){}
+//void above_average(){} // never resize, never read the file | only read the array | does not call load_scores, only the array that's filled
 
-//void below_average(){}
-
-
-
+//void below_average(){} // never resize, never read the file | only read the array | does not call load_scores, only the array that's filled
